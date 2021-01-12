619b4f8eb58c793ace59347fd60811cdcb235453
Godfadatun <danyadegoke@gmail.com>
2021-01-12 21:53:26 +0100

    stop

---
+++ thirdpty.js
@@ -0,0 +1,21 @@
+function runIframe() {
+  const clientInfo  = {
+    link:'https://app.ourpass.co/view',
+    src: 'https://raw.githubusercontent.com/Cheetah-Speed-Technology/website_dstore/master/Cap-front1.png',
+    amount: '1',
+    url: 'jumia.com.ng',
+    name:'Cap',
+    email: 'danyadegoke@gmail.com',
+    qty:'1',
+    description:'Great Pass Cap',
+    key:'pass_sec_test_APJn8wy3ggvBVqiI63TxCBSjqaOypT4l',
+    onClose: function(){
+      alert('It Closed oo')
+    },
+    onSuccess: function(){
+      alert('It Succeeded oo')
+    }
+  }
+  openIframe(clientInfo)
+
+}
