e47be5be08ce62abf2a0e24073bd52b83b248515
Godfadatun <danyadegoke@gmail.com>
2021-01-20 00:47:04 +0100

    done

--- thirdpty.js
+++ thirdpty.js
@@ -1,7 +1,7 @@
 function runIframe() {
   const clientInfo  = {
     // link:'dashboard-petra.netlify.app',
-    link: 'https://dashboard-petra.netlify.app/',
+    link: 'https://dashboard-petra.netlify.app',
     amount: 100000,
     email: 'danyadegoke@gmail.com',
     key:'sk_live_5MWv6uPNC2iq8GkQTxVPovsSz1Z9SPxK',
